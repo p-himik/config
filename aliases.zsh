4d0cad4310e03b2cd87bfb24c9fc607c97918164
eugene.pakhomov <eugene.pakhomov@xored.com>
2015-10-22 14:16:53 +0600

    Added notifications to mvn

--- aliases.zsh
+++ aliases.zsh
@@ -27,19 +27,25 @@ alias m=mimeopen
 alias e=extract
 alias f='$(thefuck $(fc -ln -1))'
 
-alias mvn="mvn-color ps-mvn"
+function _mvn_with_notify {
+    local current_path="$(pwd | sed "s/$(echo $HOME | sed 's|/|\\/|g')/~/g")"
+    mvn-color $@ && notify-info "Build Successful" "$current_path" || notify-error "Build Failed" "$current_path"
+}
+
+alias mvn='_mvn_with_notify ps-mvn'
+alias mvn2='_mvn_with_notify ps-mvn2'
+alias mvn3='_mvn_with_notify "ps-mvn -m mvn3"'
+
 alias mvnci='mvn clean install'
 alias mvni='mvn install'
 alias mvnc='mvn clean'
 alias mvnt='mvn test'
 
-alias mvn2="mvn-color ps-mvn2"
 alias mvn2ci='mvn2 clean install'
 alias mvn2i='mvn2 install'
 alias mvn2c='mvn2 clean'
 alias mvn2t='mvn2 test'
 
-alias mvn3='mvn-color "ps-mvn -m mvn3"'
 alias mvn3ci='mvn3 clean install'
 alias mvn3i='mvn3 install'
 alias mvn3c='mvn3 clean'
