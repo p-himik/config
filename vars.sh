87403bd3da6ea00b0cd3ee64a50d4a4cc802bf4a
p-himik <p1himik@gmail.com>
2016-06-29 12:50:55 +0600

    Updated some env variables

--- vars.sh
+++ vars.sh
@@ -2,13 +2,15 @@
 
 export JAVA6_HOME="/opt/jdk1.6.0_45"
 export JAVA7_HOME="/opt/jdk1.7.0_67"
-export JAVA8_HOME="/opt/jdk1.8.0_25"
+export JAVA8_HOME="/opt/jdk1.8.0_91"
 export JAVA_HOME="${JAVA7_HOME}"
 export JDK_HOME="${JAVA_HOME}"
 export IDEA_JDK="${JAVA8_HOME}"
 export PYCHARM_JDK="${JAVA8_HOME}"
 export JD_GUI_HOME="$HOME/soft/jd-gui"
-export PATH="$HOME/bin:${JAVA_HOME}/bin:${JD_GUI_HOME}:$PATH"
+export HEROKU_HOME="/usr/local/heroku"
+export ANACONDA3_HOME="$HOME/soft/anaconda3"
+export PATH="$HOME/bin:${JAVA_HOME}/bin:${JD_GUI_HOME}:${HEROKU_HOME}/bin:${ANACONDA3_HOME}/bin:$PATH"
 export GIT_ROOT="$HOME/dev/git"
 export WINEARCH="win32"
 
