361bc65c0c66930fc81bff71efc5cfd7da69d572
p-himik <p1himik@gmail.com>
2016-10-02 12:52:46 +0700

    Remove a couple of old vars

--- zsh/vars.sh
+++ zsh/vars.sh
@@ -2,14 +2,12 @@
 # This file should also be sourced in .xprofile
 # That's why it's .sh and not .zsh
 
-export JAVA6_HOME="/opt/jdk1.6.0_45"
 export JAVA7_HOME="/opt/jdk1.7.0_67"
 export JAVA8_HOME="/opt/jdk1.8.0_91"
 export JAVA_HOME="${JAVA7_HOME}"
 export JDK_HOME="${JAVA_HOME}"
 export IDEA_JDK="${JAVA8_HOME}"
 export PYCHARM_JDK="${JAVA8_HOME}"
-export JD_GUI_HOME="$HOME/soft/jd-gui"
 export HEROKU_HOME="/usr/local/heroku"
 export ANACONDA3_HOME="$HOME/soft/anaconda3"
 export PATH="$HOME/bin:$HOME/.local/bin:${JAVA_HOME}/bin:$PATH:${JD_GUI_HOME}:${HEROKU_HOME}/bin:${ANACONDA3_HOME}/bin"
