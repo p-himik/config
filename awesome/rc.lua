77a56fedde950066e26c71c73b0bb9f56d564fa1
p-himik <p1himik@gmail.com>
2016-10-09 12:42:38 +0700

    Replace rc symlink with the whole folder, add cpu widget

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -9,15 +9,16 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 -- Notification library
 local naughty = require("naughty")
+local vicious = require("vicious")
 
 -- In case we use symlink as rc.lua, we need to change paths
 local posix = require("posix")
 local _orig_getdir = awful.util.getdir
 awful.util.getdir = function(d)
     local orig = _orig_getdir(d)
-    if d == "config" then
-        orig = posix.realpath(posix.dirname(posix.readlink(orig .. '/rc.lua')))
-    end
+    --if d == "config" then
+    --    orig = posix.realpath(posix.dirname(posix.readlink(orig .. '/rc.lua')))
+    --end
     return orig
 end
 
@@ -266,6 +267,16 @@ function small_margin(widget)
 end
 
 
+cpuwidget = awful.widget.graph()
+-- Graph properties
+cpuwidget:set_width(50)
+cpuwidget:set_background_color("#494B4F")
+cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
+                    {1, "#AECF96" }}})
+-- Register widget
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+
+
 for s = 1, screen.count() do
     -- Create a promptbox for each screen
     mypromptbox[s] = awful.widget.prompt()
@@ -305,6 +316,7 @@ for s = 1, screen.count() do
             layout = wibox.layout.fixed.horizontal,
             small_margin(pomodoro.icon_widget),
             small_margin(pomodoro.widget),
+            small_margin(cpuwidget),
             APW,
             small_margin(obvious.battery()),
             small_margin(vpnwidget),
