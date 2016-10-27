3f77fbe811232d041c1ae5d37f921344534ad4ca
p-himik <p1himik@gmail.com>
2016-10-27 14:54:16 +0700

    Set Java8 as default, clean up PATH

--- zsh/vars.sh
+++ zsh/vars.sh
@@ -4,13 +4,13 @@
 
 export JAVA7_HOME="/opt/jdk1.7.0_67"
 export JAVA8_HOME="/opt/jdk1.8.0_91"
-export JAVA_HOME="${JAVA7_HOME}"
+export JAVA_HOME="${JAVA8_HOME}"
 export JDK_HOME="${JAVA_HOME}"
 export IDEA_JDK="${JAVA8_HOME}"
 export PYCHARM_JDK="${JAVA8_HOME}"
 export HEROKU_HOME="/usr/local/heroku"
 export ANACONDA3_HOME="$HOME/soft/anaconda3"
-export PATH="$HOME/bin:$HOME/.local/bin:${JAVA_HOME}/bin:$PATH:${JD_GUI_HOME}:${HEROKU_HOME}/bin:${ANACONDA3_HOME}/bin"
+export PATH="$HOME/bin:$HOME/.local/bin:${JAVA_HOME}/bin:$PATH:${HEROKU_HOME}/bin:${ANACONDA3_HOME}/bin"
 export GIT_ROOT="$HOME/dev/git"
 export WINEARCH="win32"
 
