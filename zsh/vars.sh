75df55a8d5e4479eea7f6e506bccdd082640ba62
p-himik <p1himik@gmail.com>
2017-01-12 11:43:50 +0700

    Remove some hard-coded env vars

--- zsh/vars.sh
+++ zsh/vars.sh
@@ -2,13 +2,7 @@
 # This file should also be sourced in .xprofile
 # That's why it's .sh and not .zsh
 
-export JAVA7_HOME="/opt/jdk1.7.0_67"
-export JAVA8_HOME="/opt/jdk1.8.0_91"
-export JAVA_HOME="${JAVA8_HOME}"
-export JDK_HOME="${JAVA_HOME}"
-export IDEA_JDK="${JAVA8_HOME}"
-export PYCHARM_JDK="${JAVA8_HOME}"
-export PATH="$HOME/bin:$HOME/.local/bin:${JAVA_HOME}/bin:$PATH:${HEROKU_HOME}/bin"
+export PATH="$HOME/bin:$HOME/.local/bin:${JAVA_HOME}/bin:$PATH"
 export GIT_ROOT="$HOME/dev/git"
 export WINEARCH="win32"
 
