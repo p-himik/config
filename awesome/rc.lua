93f991d98f24f87b45fa8838569bc64aad9b3b29
p-himik <p1himik@gmail.com>
2017-01-12 11:44:57 +0700

    Make Awesome clients remember their position when switching screens

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -778,4 +778,10 @@ end
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+-- this adds the default 3.5 behavior when adding/removing a screen
+-- see https://github.com/awesomeWM/awesome/issues/1382
+screen.connect_signal("removed", awesome.restart)
+screen.connect_signal("added", awesome.restart)
+
 -- }}}
