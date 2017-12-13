1adc932329ba8ae19b1c25870416970672a83d8b
p-himik <p1himik@gmail.com>
2017-12-13 18:57:13 +0700

    Remove vars.sh as they're mainly platform-based

--- zsh/vars.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-# This file should also be sourced in .xprofile
-# That's why it's .sh and not .zsh
-
-export PATH="$HOME/bin:$HOME/.local/bin:${JAVA_HOME}/bin:$PATH"
-export GIT_ROOT="$HOME/dev/git"
-export WINEARCH="win32"
-
