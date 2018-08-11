5febe8242c62d52068a18ef683b1898db9a539cc
p-himik <p1himik@gmail.com>
2018-08-11 21:37:47 +0700

    Update Awesome WM theme

--- awesome/themes/p-himik/theme.lua
+++ awesome/themes/p-himik/theme.lua
@@ -1,23 +1,40 @@
 --{{{ Main
-local awful = require("awful")
-awful.util = require("awful.util")
+local gfs = require("gears.filesystem")
 
-theme = {}
-
-function script_path()
+local function script_path()
     local str = debug.getinfo(2, "S").source:sub(2)
     return str:match("(.*/)")
 end
 
-shared        = "/usr/share/awesome"
-if not awful.util.file_readable(shared .. "/icons/awesome16.png") then
+local themedir      = script_path()
+
+local shared        = "/usr/share/awesome"
+if not gfs.file_readable(shared .. "/icons/awesome16.png") then
     shared    = "/usr/local/share/awesome"
 end
-sharedicons   = shared .. "/icons"
-sharedthemes  = shared .. "/themes"
-themedir      = script_path()
+local sharedicons   = shared .. "/icons"
+local sharedthemes  = shared .. "/themes"
+local theme_name    = "zenburn"
+local shared_theme  = sharedthemes .. "/" .. theme_name
+local awesome_dir   = themedir .. "/../.."
 
-theme.wallpaper = themedir .. '/abstract_clouds.jpg'
+local theme = {}
+
+local home_dir = os.getenv('HOME')
+local supported_extensions = {'jpg', 'png' }
+theme.wallpaper = function(_)
+    local wp_path = os.getenv('AWESOME_WALLPAPER')
+    if wp_path and gfs.file_readable(wp_path) then
+        return wp_path
+    end
+    for _, ext in ipairs(supported_extensions) do
+        wp_path = home_dir .. '.wallpaper.' .. ext
+        if gfs.file_readable(wp_path) then
+            return wp_path
+        end
+    end
+    return themedir .. '/abstract_clouds.jpg'
+end
 
 --}}}
 
@@ -34,31 +51,35 @@ theme.fg_urgent     = "#111111"
 theme.border_width  = 3
 -- theme.border_normal = "#333333"
 theme.border_normal = "#4e4e4e"
-theme.border_focus  = "#1279bf"
+--theme.border_focus  = "#1279bf"
+theme.border_focus  = "#22a4ff"
 
 theme.taglist_squares_sel         = themedir .. "/tasklist_f.png"
 theme.taglist_squares_unsel       = themedir .. "/tasklist_u.png"
 theme.tasklist_floating_icon      = themedir .. "/floating.png"
 
-theme.titlebar_close_button_normal = sharedthemes .. "/default/titlebar/close.png"
-theme.titlebar_close_button_focus  = sharedthemes .. "/default/titlebar/closer.png"
+theme.titlebar_close_button_normal = shared_theme .. "/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = shared_theme .. "/titlebar/close_focus.png"
 
 theme.menu_submenu_icon = sharedthemes .. "/default/submenu.png"
 theme.menu_height   = 15
 theme.menu_width    = 100
 
-theme.layout_fairh = "/usr/local/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/local/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/local/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_magnifier = "/usr/local/share/awesome/themes/default/layouts/magnifierw.png"
-theme.layout_max = "/usr/local/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/local/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/local/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/local/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/local/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/local/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/local/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/local/share/awesome/themes/default/layouts/dwindlew.png"
+local layouts_path = shared_theme .. "/layouts"
+theme.layout_fairh = layouts_path .. "/fairh.png"
+theme.layout_fairv = layouts_path .. "/fairv.png"
+theme.layout_floating  = layouts_path .. "/floating.png"
+theme.layout_magnifier = layouts_path .. "/magnifier.png"
+theme.layout_max = layouts_path .. "/max.png"
+theme.layout_fullscreen = layouts_path .."/fullscreen.png"
+theme.layout_tilebottom = layouts_path .. "/tilebottom.png"
+theme.layout_tileleft   = layouts_path .. "/tileleft.png"
+theme.layout_tile = layouts_path .. "/tile.png"
+theme.layout_tiletop = layouts_path .. "/tiletop.png"
+theme.layout_spiral  = layouts_path .. "/spiral.png"
+theme.layout_dwindle = layouts_path .. "/dwindle.png"
+
+theme.layout_centerwork = awesome_dir .. "/lain/icons/layout/" .. theme_name .. "/centerwork.png"
 
 theme.awesome_icon = sharedicons .. "/awesome16.png"
 
