efcf01a9add8f4d779f54736035a837ed543e7f4
p-himik <p1himik@gmail.com>
2018-08-11 21:35:47 +0700

    Change some keybindings

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -404,82 +404,58 @@ local globalkeys = gears.table.join(
     awful.key({ modkey, }, "Escape", awful.tag.history.restore,
         { description = "go back", group = "tag" }),
 
-    awful.key({}, "#107", function() -- PrtSc
-        awful.util.spawn(screenshot_screen)
+    awful.key({}, "Print", function()
+        awful.spawn(screenshot_screen)
     end),
 
-    awful.key({ "Shift" }, "#107", function() -- PrtSc
-        awful.util.spawn(screenshot_window)
+    awful.key({ "Shift" }, "Print", function()
+        awful.spawn(screenshot_window)
     end),
 
-    awful.key({ "Control", "Shift" }, "#107", function() -- PrtSc
-        awful.util.spawn(screenshot_selection)
+    awful.key({ "Control", "Shift" }, "Print", function()
+        awful.spawn(screenshot_selection)
     end),
 
-    awful.key({ modkey, "Control" }, "#46", function()
-        awful.util.spawn(lock_cmd)
+    awful.key({ modkey }, "q", function()
+        awful.spawn(lock_cmd)
     end),
 
-    awful.key({ modkey, "Control", "Shift" }, "#46", function() -- l
-        awful.util.spawn(logout_cmd)
+    awful.key({ modkey, "Mod1" }, "q", function()  -- Alt
+        awful.spawn(logout_cmd)
     end),
 
-    awful.key({ modkey, "Control" }, "#39", function() -- s
-        awful.util.spawn_with_shell(suspend_cmd)
+    awful.key({ modkey, "Control" }, "q", function()
+        awful.spawn.with_shell(suspend_cmd)
     end),
 
-    awful.key({ modkey, "Control" }, "#43", function() -- h
-        awful.util.spawn(hibernate_cmd)
+    awful.key({ modkey, "Control", "Shift" }, "q", function()
+        awful.spawn(hibernate_cmd)
     end),
 
-    awful.key({ modkey, }, "j",
-        function()
-            awful.client.focus.byidx(1)
-        end,
-        { description = "focus next by index", group = "client" }),
-    awful.key({ modkey, }, "k",
-        function()
-            awful.client.focus.byidx(-1)
-        end,
-        { description = "focus previous by index", group = "client" }),
     awful.key({ modkey, }, "w", function() mymainmenu:show() end,
         { description = "show main menu", group = "awesome" }),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift" }, "j", function() awful.client.swap.byidx(1) end,
-        { description = "swap with next client by index", group = "client" }),
-    awful.key({ modkey, "Shift" }, "k", function() awful.client.swap.byidx(-1) end,
-        { description = "swap with previous client by index", group = "client" }),
     awful.key({ modkey, "Control" }, "j", function() awful.screen.focus_relative(1) end,
         { description = "focus the next screen", group = "screen" }),
     awful.key({ modkey, "Control" }, "k", function() awful.screen.focus_relative(-1) end,
         { description = "focus the previous screen", group = "screen" }),
     awful.key({ modkey, }, "u", awful.client.urgent.jumpto,
         { description = "jump to urgent client", group = "client" }),
-    awful.key({ modkey, }, "Tab",
-        function()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end,
-        { description = "go back", group = "client" }),
 
     -- Standard program
     awful.key({ modkey, }, "Return", function() awful.spawn(terminal) end,
         { description = "open a terminal", group = "launcher" }),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
         { description = "reload awesome", group = "awesome" }),
-    awful.key({ modkey, "Shift" }, "q", awesome.quit,
-        { description = "quit awesome", group = "awesome" }),
 
-    awful.key({ modkey, }, "l", function() awful.tag.incmwfact(0.05) end,
+    awful.key({ modkey }, ".", function() awful.tag.incmwfact(0.05) end,
         { description = "increase master width factor", group = "layout" }),
-    awful.key({ modkey, }, "h", function() awful.tag.incmwfact(-0.05) end,
+    awful.key({ modkey }, ",", function() awful.tag.incmwfact(-0.05) end,
         { description = "decrease master width factor", group = "layout" }),
-    awful.key({ modkey, "Shift" }, "h", function() awful.tag.incnmaster(1, nil, true) end,
+    awful.key({ modkey, "Control", "Shift" }, "h", function() awful.tag.incnmaster(1, nil, true) end,
         { description = "increase the number of master clients", group = "layout" }),
-    awful.key({ modkey, "Shift" }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
+    awful.key({ modkey, "Control", "Shift" }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
         { description = "decrease the number of master clients", group = "layout" }),
     awful.key({ modkey, "Control" }, "h", function() awful.tag.incncol(1, nil, true) end,
         { description = "increase the number of columns", group = "layout" }),
@@ -520,6 +496,27 @@ local globalkeys = gears.table.join(
         { description = "show the menubar", group = "launcher" })
 )
 --@formatter:on
+
+local client_direction_keys = {
+    { key = "j", dir = "down", desc = "below" },
+    { key = "k", dir = "up", desc = "above" },
+    { key = "h", dir = "left", desc = "on the left" },
+    { key = "l", dir = "right", desc = "on the right" }
+}
+for _, k_desc in pairs(client_direction_keys) do
+    globalkeys = gears.table.join(globalkeys,
+        awful.key({ modkey }, k_desc.key,
+            function()
+                awful.client.focus.bydirection(k_desc.dir)
+                if client.focus then client.focus:raise() end
+            end,
+            { description = "focus a client " .. k_desc.desc, group = "client" }),
+        awful.key({ modkey, "Shift" }, k_desc.key,
+            function()
+                awful.client.swap.bydirection(k_desc.dir)
+            end,
+            { description = "swap with a client " .. k_desc.desc, group = "client" }))
+end
 
 local function show_client_under_mouse_properties(c)
     local f = io.popen('xdotool getmouselocation --shell | grep WINDOW |cut -d= -f2')
