1e9f32d2ae7dbe923241df841ae300cf6836f5f8
Eugene Pakhomov <p1himik@gmail.com>
2021-05-03 19:03:59 +0300

    Rewrite vpnwidget using awful.widget.watch

--- awesome/vpnwidget.lua
+++ awesome/vpnwidget.lua
@@ -1,25 +1,17 @@
-local textbox = require("wibox.widget.textbox")
-local timer = require("gears.timer")
-local execute = os.execute
+local awful = require('awful')
 local setmetatable = setmetatable
 
 local vpnwidget = { mt = {} }
 
 function vpnwidget.new(timeout)
-    local widget = textbox("VPN: N/A")
-    local t = timer { timeout = timeout or 5 }
-
-    t:connect_signal("timeout", function()
-        local _, _, exit_code = execute('test ! -z "`ip tuntap show 2>/dev/null | grep -v ^vir`"')
-        if exit_code == 0 then
+    local cmd = {awful.util.shell, '-c', 'ip tuntap show 2>/dev/null | grep -v ^vir' }
+    return awful.widget.watch(cmd, timeout or 5, function(widget, result)
+        if result ~= '' then
             widget:set_markup(" <span color='#00FF00'>VPN: ON</span> ")
         else
             widget:set_markup(" <span color='#FF0000'>VPN: OFF</span> ")
         end
     end)
-    t:start()
-    t:emit_signal("timeout")
-    return widget
 end
 
 function vpnwidget.mt.__call(_, ...)
