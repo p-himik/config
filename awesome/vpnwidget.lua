5bc8c611b1feb1ae53cd74febebb17c061c9b2fa
Eugene Pakhomov <p1himik@gmail.com>
2025-04-23 23:48:52 +0300

    Make the VPN widget work with WireGuard as well

--- awesome/vpnwidget.lua
+++ awesome/vpnwidget.lua
@@ -4,9 +4,9 @@ local setmetatable = setmetatable
 local vpnwidget = { mt = {} }
 
 function vpnwidget.new(timeout)
-    local cmd = {awful.util.shell, '-c', 'ip tuntap show 2>/dev/null | grep -v ^vir' }
-    return awful.widget.watch(cmd, timeout or 5, function(widget, result)
-        if result ~= '' then
+    local cmd = {awful.util.shell, '-c', 'ip route get to 1.1.1.1 | grep -q "dev.*table"' }
+    return awful.widget.watch(cmd, timeout or 5, function(widget, _stdout, _stderr, _exitreason, exitcode)
+        if exitcode == 0 then
             widget:set_markup(" <span color='#00FF00'>VPN: ON</span> ")
         else
             widget:set_markup(" <span color='#FF0000'>VPN: OFF</span> ")
